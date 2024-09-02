5e6e716221b71484f65f85f77b62a7161176cab2
Mikhail Mitrofanov <mmm606@inbox.ru>
2024-09-02 12:29:11 +0300

    добавлена иконка сегодня в мой нижний бар

--- app/src/main/java/com/mimm/tehtt/MainActivity.kt
+++ app/src/main/java/com/mimm/tehtt/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mimm.tehtt.ui.theme.TehTTTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +33,51 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TehTTTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            MainView()
+        }
+    }
+}
+
+@Composable
+fun MainView() {
+    val navController = rememberNavController()
+
+    TehTTTheme {
+        Scaffold(modifier = Modifier.fillMaxSize(), bottomBar = {
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .background(Color.Gray, RoundedCornerShape(16.dp))
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Image(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.schedule),
+                        contentDescription = "",
+                        modifier = Modifier.size(40.dp)
                     )
+                }
+            }
+        }) { innerPadding ->
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                NavHost(navController = navController, startDestination = "today") {
+                    composable("today") {
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TehTTTheme {
-        Greeting("Android")
-    }
+    MainView()
 }
